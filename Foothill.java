0e2e0582d34df8388883fb49496e80b615c56d59
sheldon cooper <coopsheldonfox@ucla.edu>
2015-11-30 13:13:33 -0800

    The original commit for the password project

---
+++ Foothill.java
@@ -0,0 +1,53 @@
+/* CS 001A
+ * First Assignment
+  Elham Rasti */
+public class Foothill
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello World!");
+      // Personal Info:
+      System.out.println("---------Personal Information:---------");
+      System.out.println("My Last Name is " + "'Rasti'.");
+      System.out.println("My Foothill Id (Student Id): " + 20249213.);
+      System.out.println();
+      // Course Details: 
+      System.out.println("---------Details about Course Syllabus:---------");
+      System.out.println("1-Recommended text book:" 
+            + " " + "Introduction to Java Programming " + 
+            "Comprehensive Version" + " " + "10th edition,by Liang,Pearson.");
+      System.out.println("2-In Week 6 we will learn about OOP programming" + 
+            " " + "and instance data and method and we have Midterm Exam.");
+      System.out.println();
+      // Details about Module 2R
+      System.out.println("---------Details about Module 2R:---------");
+      System.out.println("#1-One of Style rules is: "+
+            "For methodName() and Variable use camelCase format"+
+            " and for constant variable use UPPER_CASE.");
+      System.out.println("#2-We need to remove all Tabs"+ " " +
+            "and insert with 3 Spaces.");
+      System.out.println("#3-One Factor causes point deduction is: ");
+      System.out.println(" Editing and modifying the output program(Run).");
+   }
+}
+
+/*-------------------- Out Put ---------------------
+Hello World!
+---------Personal Information:---------
+My Last Name is 'Rasti'.
+My Foothill Id (Student Id): 20249213.
+
+---------Details about Course Syllabus:---------
+1-Recommended text book: Introduction to Java Programming Comprehensive Version 
+10th edition,by Liang,Pearson.
+2-In Week 6 we will learn about OOP programming and instance data and method and
+ we have Midterm Exam.
+
+---------Details about Module 2R:---------
+#1-One of Style rules is: For methodName() and Variable use camelCase format and
+ for constant variable use UPPER_CASE.
+#2-We need to remove all Tabs and insert with 3 Spaces.
+#3-One Factor causes point deduction is: 
+ Editing and modifying the output program(Run).
+
+ */
