2d453d34b3096edc40b0c3be7a12418a3a611b88
Elyras <lastodin77@gmail.com>
2015-11-30 16:24:01 -0800

    Update Foothill.java

--- Foothill.java
+++ Foothill.java
@@ -1,6 +1,4 @@
-/* CS 001A
- * First Assignment
-  Elham Rasti */
+
 public class Foothill
 {
    public static void main(String[] args)
@@ -8,8 +6,7 @@ public class Foothill
       System.out.println("Hello World!");
       // Personal Info:
       System.out.println("---------Personal Information:---------");
-      System.out.println("My Last Name is " + "'Rasti'.");
-      System.out.println("My Foothill Id (Student Id): " + 20249213.);
+
       System.out.println();
       // Course Details: 
       System.out.println("---------Details about Course Syllabus:---------");
@@ -31,23 +28,4 @@ public class Foothill
    }
 }
 
-/*-------------------- Out Put ---------------------
-Hello World!
----------Personal Information:---------
-My Last Name is 'Rasti'.
-My Foothill Id (Student Id): 20249213.
 
----------Details about Course Syllabus:---------
-1-Recommended text book: Introduction to Java Programming Comprehensive Version 
-10th edition,by Liang,Pearson.
-2-In Week 6 we will learn about OOP programming and instance data and method and
- we have Midterm Exam.
-
----------Details about Module 2R:---------
-#1-One of Style rules is: For methodName() and Variable use camelCase format and
- for constant variable use UPPER_CASE.
-#2-We need to remove all Tabs and insert with 3 Spaces.
-#3-One Factor causes point deduction is: 
- Editing and modifying the output program(Run).
-
- */
